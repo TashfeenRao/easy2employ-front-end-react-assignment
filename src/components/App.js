cbfeb19f55617cfac87422136f6e93474000763f
tashfeenrao <raotashfeen25@gmail.com>
2020-08-29 00:41:56 +0500

    login component converted to class component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,24 @@ import Login from '../components/presentational/Login';
 import UserView from '../components/presentational/UserView';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedInStatus: "not_logged_in",
+      user: {}
+    }
+  }
   render() {
     return (
       <div>
         <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route 
+          exact 
+          path="/" 
+          render={props => (
+            <Login {... props} loggedStatus={this.state.loggedInStatus}/>
+          )} />
           <Route exact path="/userView" component={UserView} />
         </Switch>
         </BrowserRouter>
--- src/components/presentational/Login.js
+++ src/components/presentational/Login.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React,{ Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import LoginForm from '../container/LoginForm';
 import Registration from '../auth/Registration';
 
-function Login() {
-  return (
-    <Container>
-      <Row className="justify-content-center mt-4">
-          <LoginForm />
-          <Registration />
-      </Row>
-    </Container>
-  );
+export default class Login extends Component {
+
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center mt-4">
+            <LoginForm />
+            <Registration />
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default Login;
