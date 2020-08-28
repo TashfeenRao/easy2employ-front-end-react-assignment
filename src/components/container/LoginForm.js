6fb841579fa3229062f2dbab0f68387c96e095eb
tashfeenrao <raotashfeen25@gmail.com>
2020-08-29 02:37:17 +0500

    recieved login user data from api

--- src/components/container/LoginForm.js
+++ src/components/container/LoginForm.js
@@ -1,26 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
-function LoginForm() {
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      password: "",
+      registrationError: "",
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { username, password } = this.state;
+    axios
+      .post(
+        "http://localhost:3001/sessions",
+        {
+          user: {
+            username: username,
+            password: password,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log(response)
+        //if(response.data.status === "created") {
+           // this.props.handleSuccessFull(response.data)
+        //}
+      })
+      .catch((error) => {
+        console.log("login error", error);
+      });
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+  render() {
     return (
-        <div>
-          <Form>
-          <Form.Group controlId="formBasicEmail">
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
             <Form.Label>User Name</Form.Label>
-            <Form.Control type="email" placeholder="Username" />
+            <Form.Control
+              type="username"
+              name="username"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleChange}
+              required
+            />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group>
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Login
           </Button>
         </Form>
-        </div>
+      </div>
     )
+  }
 }
 
-export default LoginForm
