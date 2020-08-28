1c325cd05cb3291aac05334884322428efae4661
tashfeenrao <raotashfeen25@gmail.com>
2020-08-28 20:40:58 +0500

    installed react router and added browser router to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '../components/presentational/Login';
+import UserView from '../components/presentational/UserView';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="userView" component={UserView} />
+        </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/presentational/UserView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserView() {
+    return (
+        <div>
+            <h1>User view</h1>
+        </div>
+    )
+}
+
+export default UserView
