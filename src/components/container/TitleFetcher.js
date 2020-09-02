b71088814e9fd0cdf743d01489fafd28b4654198
tashfeenrao <raotashfeen25@gmail.com>
2020-09-02 23:23:28 +0500

    added textarea for input

--- src/components/container/TitleFetcher.js
+++ src/components/container/TitleFetcher.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
@@ -6,18 +7,35 @@ export default class TitleFetcher extends Component {
     constructor() {
         super();
 
+        this.state = {
+            title: ''
+        }
 
+        this.handleTitleForm = this.handleTitleForm.bind(this)
+        this.handleTitleChange = this.handleTitleChange.bind(this)
+    }
+
+    handleTitleForm(event) {
+        event.preventDefault()
+    }
+
+    handleTitleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
   render() {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleTitleForm}>
           <Form.Group>
             <Form.Label>URL for Website</Form.Label>
             <Form.Control
               type="inputurl"
-              name="url"
+              as="textarea"
+              name="title"
               placeholder="Enter website urls"
+              onChange={this.handleTitleChange}
               required
             />
           </Form.Group>
